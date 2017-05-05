fbe2b509ad1cf584fff90eb3c932f6b9b30b6e27
Benjamin O'Keefe <benjamin.okeefe@inrix.com>
2017-05-05 11:42:04 -0700

    Button text support for multiple languages

--- appView.js
+++ appView.js
@@ -2,7 +2,9 @@ define(function (require) {
     'use strict';
 
     const
-        ModuleView = require('common/platform/ModuleView');
+        ModuleView = require('common/platform/ModuleView'),
+        Button = require('common/ui/Button'),
+        LocaleStrings = require('common/lib/LocaleStrings');
 
     /**
      * This is the application's root view, which is used for any UI control creation and handling.
@@ -16,113 +18,55 @@ define(function (require) {
      * @see Promises {@link https://insidetrack.opencar.com/documentation/2.2/promises}
      */
     return class extends ModuleView {
+        loadI18n (languageRegionCode) {
+            return this.i18n.load('i18n', languageRegionCode);
+        }
 
-        /**
-         * Called prior to the application being displayed.
-         * Executes in parallel to the module controller's init().
-         * Controller and View are not yet linked.
-         *
-         * @async Must return a promise
-         * @override Must call super.init (ideally chaining from its promise)
-         * @returns {Promise}
-         */
-        // init () {
-        //     return super.init().then(() => {
-        //         Log.debug("view init()");
-        //     });
-        // }
+        init () {
+            return super.init(...arguments).then(() => {
+                this.i18n = new LocaleStrings();
+                this.i18n.setLocalizationBasePath(`${window.MODULE_PATH}/i18n`);
+            });
+        }
 
-        /**
-         * Controller and View are now linked
-         *
-         * @async
-         * @override super.beforeStart
-         * @returns {Promise}
-         */
-        // beforeStart () {
-        //     return super.beforeStart().then(() => {
-        //         Log.debug("view beforeStart()");
-        //     });
-        // }
+        beforeStart () {
+            return super.beforeStart().then(() => {
+                this.loadI18n('en-us').then(() => {
+                    this.renderButtons();
+                });
+            });
+        }
 
-        /**
-         * View is transitioned in and elements are visible.
-         *
-         * @async
-         * @override super.started
-         * @returns {Promise}
-         */
-        // started () {
-        //     return super.started().then(() => {
-        //         Log.debug("view started()");
-        //     });
-        // }
+        buttonClickHandler (languageRegionCode) {
+            this.loadI18n(languageRegionCode).then(() => {
+                const view = this.getView();
 
-        /**
-         * Called prior to pausing the application and placing in background
-         *
-         * @async
-         * @override super.beforePause
-         * @returns {Promise}
-         */
-        // beforePause () {
-        //     return super.beforePause().then(() => {
-        //         Log.debug("view beforePause()");
-        //     });
-        // }
+                while (view.hasChildNodes()) {
+                    view.removeChild(view.lastChild);
+                }
 
-        /**
-         * Application is paused and is now paused in background
-         *
-         * @async
-         * @override super.paused
-         * @returns {Promise}
-         */
-        // paused () {
-        //     return super.paused().then(() => {
-        //         Log.debug("view paused()");
-        //     });
-        // }
+                this.renderButtons();
+            });
+        }
 
-        /**
-         * Called prior to being reactivated from being paused in background
-         *
-         * @async
-         * @override super.beforeResume
-         * @returns {Promise}
-         */
-        // beforeResume () {
-        //     return super.beforeResume().then(() => {
-        //         Log.debug("view beforeResume()");
-        //     });
-        // }
+        renderButtons () {
+            const locales = [
+                { language: this.i18n.getString('i18n.button.english.text'), code: 'en-us' },
+                { language: this.i18n.getString('i18n.button.japanese.text'), code: 'jp-jp' },
+                { language: this.i18n.getString('i18n.button.spanish.text'), code: 'es-es' },
+                { language: this.i18n.getString('i18n.button.canada.text'), code: 'en-ca' }
+            ];
 
-        /**
-         * Application is reactivated to foreground
-         *
-         * @async
-         * @override super.resumed
-         * @returns {Promise}
-         */
-        // resumed () {
-        //     return super.resumed().then(() => {
-        //         Log.debug("view resumed()");
-        //     });
-        // }
-
-        /**
-         * Called after the application has been paused and fully transitioned out of view,
-         * but prior to the module being stopped and removed from runtime.
-         *
-         * @async
-         * @override super.beforeStop
-         * @returns {Promise}
-         */
-        // beforeStop () {
-        //     return super.beforeStop().then(() => {
-        //         Log.debug("view beforeStop()");
-        //     });
-        // }
-
+            locales.forEach(locale => {
+                new Button({
+                    model: {
+                        text: locale.language
+                    },
+                    click: () => {
+                        this.buttonClickHandler(locale.code);
+                    }
+                }).render(this.getView());
+            });
+        }
     };
 });
