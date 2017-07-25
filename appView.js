8aaf8f96e434006bf2d92792519fbbf50bc20e0b
Benjamin O'Keefe <benjamin.okeefe@inrix.com>
2017-07-25 10:28:55 -0700

    Removed i18n handling from app

--- appView.js
+++ appView.js
@@ -20,49 +20,13 @@ define(function (require) {
      * @see Promises {@link https://insidetrack.opencar.com/documentation/2.2/promises}
      */
     return class extends ModuleView {
-        loadI18n (languageRegionCode) {
-            this.i18n.table.clear()
-            return this.i18n.load('i18n', languageRegionCode);
-        }
-
-        init () {
-            return super.init(...arguments).then(() => {
-                this.i18n = new LocaleStrings();
-                this.i18n.setLocalizationBasePath(`${window.MODULE_PATH}/i18n`);
-            });
-        }
-
         beforeStart () {
-            return super.beforeStart().then(() => {
-                this.loadI18n('en-us').then(() => {
-                    this.renderUI();
-                });
-            });
+            this.renderUI();
         }
 
         buttonClickHandler (languageRegionCode) {
             this.loadI18n(languageRegionCode).then(() => {
                 this.renderUI();
-            });
-        }
-
-        renderButtons () {
-            const locales = [
-                { language: this.i18n.getString('i18n.button.english.text'), code: 'en-us' },
-                { language: this.i18n.getString('i18n.button.japanese.text'), code: 'jp-jp' },
-                { language: this.i18n.getString('i18n.button.spanish.text'), code: 'es-es' },
-                { language: this.i18n.getString('i18n.button.canada.text'), code: 'en-ca' }
-            ];
-
-            locales.forEach(locale => {
-                new Button({
-                    model: {
-                        text: locale.language
-                    },
-                    click: () => {
-                        this.buttonClickHandler(locale.code);
-                    }
-                }).render(this.getView());
             });
         }
 
@@ -98,8 +62,7 @@ define(function (require) {
             while (view.hasChildNodes()) {
                 view.removeChild(view.lastChild);
             }
-
-            this.renderButtons();
+            
             this.renderWelcomeMessage();
             this.renderEnglishLanguageMessage();
             this.renderFlagImage();
